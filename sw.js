baa64c631aa1de4151e380934401c4613227a76c
haidaifalai <1071811881@qq.com>
2026-08-13 16:59:41 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'aquatrack-v1';
+const ASSETS_TO_CACHE = [
+  './aquatrack.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
